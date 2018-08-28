4a6cb54017da5b1524650bce2eaeb3fc708be529
EndlessEden <eenov1988@gmail.com>
2018-08-28 19:22:03 +1000

    Revert "OpenCL: Drop "allow CPU" option"
    This reverts commit a64f3d5d2c2fd89d8b3764a84a376d8de802b8d9.

--- libethcore/EthashCPUMiner.h
+++ libethcore/EthashCPUMiner.h
@@ -40,9 +40,33 @@ public:
 
 	static unsigned instances() { return s_numInstances > 0 ? s_numInstances : std::thread::hardware_concurrency(); }
 	static std::string platformInfo();
+<<<<<<< HEAD:libethcore/EthashCPUMiner.h
 	static void listDevices() {}
 	static bool configureGPU(unsigned, unsigned, unsigned, unsigned, unsigned, bool, unsigned, uint64_t) { return false; }
 	static void setNumInstances(unsigned _instances) { s_numInstances = std::min<unsigned>(_instances, std::thread::hardware_concurrency()); }
+=======
+	static unsigned getNumDevices();
+	static void listDevices();
+	static bool configureGPU(
+		unsigned _localWorkSize,
+		unsigned _globalWorkSizeMultiplier,
+		unsigned _platformId,
+		unsigned _deviceId,
+		bool _allowCPU,
+		unsigned _extraGPUMemory,
+		uint64_t _currentBlock,
+		unsigned _dagLoadMode,
+		unsigned _dagCreateDevice
+	);
+	static void setNumInstances(unsigned _instances) { s_numInstances = std::min<unsigned>(_instances, getNumDevices()); }
+	static void setDevices(unsigned * _devices, unsigned _selectedDeviceCount)
+	{
+		for (unsigned i = 0; i < _selectedDeviceCount; i++)
+		{
+			s_devices[i] = _devices[i];
+		}
+	}
+>>>>>>> parent of a64f3d5d2... OpenCL: Drop "allow CPU" option:libethcore/EthashGPUMiner.h
 
 protected:
 	void kickOff() override;
